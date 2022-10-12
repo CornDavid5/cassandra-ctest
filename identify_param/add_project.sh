655e655e5e1e091538a20fb8c6c874293f8969bb
Pinhao.G <davidgph.1@gmail.com>
2022-10-12 12:32:01 -0500

    Move thing around

--- identify_param/add_project.sh
+++ identify_param/add_project.sh
@@ -50,7 +50,7 @@ function setup_alluxio() {
 function setup_cassandra() {
     [ ! -d "app/ctest-alluxio" ] && git clone https://github.com/apache/cassandra.git app/cassandra
     cd app/cassandra
-    git fetch && git switch cassandra-4.0
+    git checkout 4e1d31e
     git apply ../../ctest-logging.patch
     CASSANDRA_USE_JDK11=true ant
 }
